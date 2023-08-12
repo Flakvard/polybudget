282feb43f4849f7b01777c09be553460c83f5f01
flakvard <marnifalkvard@gmail.com>
2023-08-12 23:04:17 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold( // layout widget to flesh out general layout of the app. Check scaffold class in docs.
-      appBar: AppBar(
-        title: const Text('PolyBudget'),
-        centerTitle: true,
-        backgroundColor: Colors.pink[900],
-      ),
-      body: const Center(
-        child: Text(
-          'Hello my dudes',
-          style: TextStyle(
-              fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.grey,
-            fontFamily: 'IndieFlower'
-          )
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        backgroundColor: Colors.pink[900],
-        child: const Text('Click'),
-      )
-    ),
+    home: Home()
   ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( // layout widget to flesh out general layout of the app. Check scaffold class in docs.
+        appBar: AppBar(
+          title: const Text('PolyBudget'),
+          centerTitle: true,
+          backgroundColor: Colors.pink[900],
+        ),
+        body: const Center(
+          child: Text(
+              'Hello my dudes',
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                  fontFamily: 'IndieFlower'
+              )
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {  },
+          backgroundColor: Colors.pink[900],
+          child: const Text('Click'),
+        )
+    );
+  }
 }
 
