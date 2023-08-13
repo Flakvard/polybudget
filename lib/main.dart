8e53ee824cc933ee6fbf175628fd153e01db6e83
flakvard <marnifalkvard@gmail.com>
2023-08-13 11:18:10 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'user.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -6,9 +7,15 @@ void main() {
   ));
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold( // layout widget to flesh out general layout of the app. Check scaffold class in docs.
@@ -17,86 +24,23 @@ class Home extends StatelessWidget {
           title: const Text('PolyBudget'),
           centerTitle: true,
           backgroundColor: Colors.pink[300],
-          elevation: 0.0, // removes shadows
+          elevation: 0.0, // removes shadow
         ),
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/avatar.jpg'),
-                  radius: 40.0,
-                ),
+        body: Center(
+            child: ElevatedButton(
+             onPressed: () {
+               // Navigate to User Page when the button is clicked
+               Navigator.push(
+                 context,
+                 MaterialPageRoute(builder: (context) => User()),
+               );
+             },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.pink[300],
               ),
-              Divider(
-                height: 60.0,
-                color: Colors.grey[800],
-              ),
-              const Text(
-                'NAME',
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              SizedBox(height: 10.0),
-              Text(
-                'Marni',
-                style: TextStyle(
-                  color: Colors.pink[300],
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10.0),
-              const Text(
-                'CURRENT LEVEL',
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              SizedBox(height: 10.0),
-              Text(
-                '8',
-                style: TextStyle(
-                  color: Colors.pink[300],
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 30.0),
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.email,
-                    color: Colors.grey[400],
-                  ),
-                  SizedBox(width: 10.0),
-                  Text(
-                    'marni@email.com',
-                    style: TextStyle(
-                      color: Colors.grey[400],
-                      fontSize: 18.0,
-                      letterSpacing: 1.0,
-                    ),
-                  )
-                ],
-              ),
-            ],
-          ),
-
+              child: const Text('User Page'),
+           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {  },
-          backgroundColor: Colors.pink[300],
-          elevation: 0.0,
-          child: const Text('Click'),
-        )
     );
   }
 }
---
+++ lib/user.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class User extends StatefulWidget {
+  const User({super.key});
+
+
+  @override
+  State<User> createState() => _UserState();
+}
+
+class _UserState extends State<User> {
+  int currentLevel = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( // layout widget to flesh out general layout of the app. Check scaffold class in docs.
+        backgroundColor: Colors.grey.shade900,
+        appBar: AppBar(
+          title: const Text('PolyBudget'),
+          centerTitle: true,
+          backgroundColor: Colors.pink[300],
+          elevation: 0.0, // removes shadow
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/avatar.jpg'),
+                  radius: 40.0,
+                ),
+              ),
+              Divider(
+                height: 60.0,
+                color: Colors.grey[800],
+              ),
+              const Text(
+                'NAME',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Marni',
+                style: TextStyle(
+                  color: Colors.pink[300],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              const Text(
+                'CURRENT LEVEL',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                '$currentLevel',
+                style: TextStyle(
+                  color: Colors.pink[300],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    'marni@email.com',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              ++currentLevel;
+            });
+          },
+          backgroundColor: Colors.pink[300],
+          elevation: 0.0,
+          child: const Text('Click'),
+        )
+    );
+  }
+}
+
