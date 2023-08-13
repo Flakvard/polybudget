60890839432a1f729bc541c54f9fc49f9b76ab15
flakvard <marnifalkvard@gmail.com>
2023-08-13 01:47:09 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,89 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold( // layout widget to flesh out general layout of the app. Check scaffold class in docs.
+        backgroundColor: Colors.grey.shade900,
         appBar: AppBar(
           title: const Text('PolyBudget'),
           centerTitle: true,
           backgroundColor: Colors.pink[300],
+          elevation: 0.0, // removes shadows
         ),
-        body: Row(),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/avatar.jpg'),
+                  radius: 40.0,
+                ),
+              ),
+              Divider(
+                height: 60.0,
+                color: Colors.grey[800],
+              ),
+              const Text(
+                'NAME',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Marni',
+                style: TextStyle(
+                  color: Colors.pink[300],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              const Text(
+                'CURRENT LEVEL',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                '8',
+                style: TextStyle(
+                  color: Colors.pink[300],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    'marni@email.com',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {  },
           backgroundColor: Colors.pink[300],
+          elevation: 0.0,
           child: const Text('Click'),
         )
     );
