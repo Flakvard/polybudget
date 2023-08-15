38af3f414bd69a690be31c6b478404b09eee1c4c
Marni <marni@falkvard.com>
2023-08-15 13:57:59 +0200

    update refactor

--- lib/transactionList.dart
+++ lib/transactionList.dart
@@ -1,60 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:polybudget/transactions.dart';
-
-class TransactionCardContent extends StatelessWidget {
-  final Transaction transaction;
-  // constructor
-  // const TransactionCardContent(this.transaction, {Key? key}) : super(key: key);
-  const TransactionCardContent({super.key, required this.transaction});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              transaction.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            const SizedBox(height: 6.0),
-            Text(
-              // double.parse(transaction.total).toString(),
-              transaction.total.toString(),
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.red[600],
-              ),
-            ),
-            const SizedBox(height: 6.0),
-            Text(
-              transaction.category,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[800],
-              ),
-            ),
-            const SizedBox(height: 6.0),
-            Text(
-              transaction.date,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
+import 'package:polybudget/TransactionCardContent.dart';
 
 class TransactionList extends StatefulWidget {
   const TransactionList({super.key});
