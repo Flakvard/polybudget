67119b6623b2298a9f90d2d567db5bc0747150b8
Marni <marni@falkvard.com>
2023-08-18 12:40:29 +0200

    refactor and added db QuerySnapshot

--- lib/database.dart
+++ lib/database.dart
@@ -8,13 +8,54 @@ class DatabaseService {
   // Collection reference
   final CollectionReference polyBudgetDB = FirebaseFirestore.instance.collection('pbUsers');
 
-  Future updateUserData(String name) async {
+  Future updateUserData(String name, String email, String budgetName, String categories,) async {
     return await polyBudgetDB
         .doc(uid)
         .set({
-          'name': name,
+            "username": name,
+            "email": email,
+            "budgets": {
+              "budget_id": {
+                "name": budgetName,
+                "categories": {
+                  "category_id_1": {
+                    "name": categories,
+                  }
+                },
+                "bankAccounts": {
+                  "account_id_1": {
+                    "name": "Savings Account",
+                    "balance": 5000.0
+                  },
+                  "account_id_2": {
+                    "name": "Checking Account",
+                    "balance": 1500.0
+                  }
+                },
+                "transactions": {
+                  "transaction_id_1": {
+                    "type": "income",
+                    "amount": 100.0,
+                    "category": "category_id_1",
+                    "date": "2023-08-15"
+                  },
+                  "transaction_id_2": {
+                    "type": "expense",
+                    "amount": 50.0,
+                    "category": "category_id_2",
+                    "date": "2023-08-17"
+                  }
+                }
+              }
+            }
         });
   }
+  // returns a snapshot of the db
+  Stream<QuerySnapshot<Object?>> get myUsers {
+    return polyBudgetDB.snapshots();
+  }
+
+
   // late String text;
   // late double total;
   // late String category;
