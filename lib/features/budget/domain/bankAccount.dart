282503391223c97788487d49fbfb385ee760ab2d
Marni <marni@falkvard.com>
2023-08-19 12:47:58 +0200

    Create bankAccount.dart

---
+++ lib/features/budget/domain/bankAccount.dart
@@ -0,0 +1,12 @@
+
+class BankAccount {
+  final String id;
+  final String name;
+  final double balance;
+
+  BankAccount({
+    required this.id,
+    required this.name,
+    required this.balance,
+  });
+}
