c0837561681592f7ba426a04bfa6765c7ab56a7a
Marni <marni@falkvard.com>
2023-08-24 23:30:24 +0200

    update

--- lib/features/budget/domain/budget.dart
+++ lib/features/budget/domain/budget.dart
@@ -7,6 +7,12 @@ class Budget {
   Budget({required this.id, required this.name});
 
 
+  factory Budget.fromJson(Map<String, dynamic> data) {
+    // ! there's a problem with this code (see below)
+    final id = data['id'];
+    final name = data['name'];
+    return Budget(id: id, name: name);
+  }
 
 
 }
