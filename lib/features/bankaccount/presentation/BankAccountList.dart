971fa48a9e198effa938d45ad8df1ff33807f943
Marni <marni@falkvard.com>
2023-08-30 10:22:22 +0200

    inject home wrapper

--- lib/features/bankaccount/presentation/BankAccountList.dart
+++ lib/features/bankaccount/presentation/BankAccountList.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:polybudget/common_widgets/presentation/home_wrapper.dart';
 import 'package:polybudget/features/bankaccount/domain/bankAccount.dart';
 import 'package:provider/provider.dart';
 import 'bankAccountsTile.dart';
@@ -11,13 +12,20 @@ class BankAccountList extends StatelessWidget {
 
     final bankAccounts = Provider.of<List<BankAccount?>?>(context) ?? [];
 
-    return ListView.builder(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      itemCount: bankAccounts?.length,
-      itemBuilder: (context, index){
-        return BankAccountTile(bankAccount: bankAccounts![index]);
-      },
+    return HomeWrapper(
+      content: Column(
+        children: [
+          const SizedBox(height: 12.0,),
+          ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: bankAccounts?.length,
+            itemBuilder: (context, index){
+              return BankAccountTile(bankAccount: bankAccounts![index]);
+            },
+          ),
+        ],
+      )
     );
   }
 }
