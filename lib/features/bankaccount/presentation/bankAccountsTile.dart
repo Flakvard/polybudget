fde86d34f5891a93c25c3a6b9e8cf670a496ffe8
Marni <marni@falkvard.com>
2023-08-31 16:39:53 +0200

    dropdown pr bankaccount line tile

--- lib/features/bankaccount/presentation/bankAccountsTile.dart
+++ lib/features/bankaccount/presentation/bankAccountsTile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:polybudget/features/bankaccount/domain/bankAccount.dart';
 
+import '../../../common_widgets/presentation/constants.dart';
+
 class BankAccountTile extends StatelessWidget {
 
   final BankAccount? bankAccount;
@@ -9,6 +11,10 @@ class BankAccountTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final List<String> sugars = ['Delete','Update']; // dropdown values
+
+    String? errorMsg = "";
 
     return Padding(
       padding: const EdgeInsets.only(top: 8.0),
@@ -21,10 +27,40 @@ class BankAccountTile extends StatelessWidget {
           ),
           title: Text(bankAccount!.name),
           subtitle: Text('id: ${bankAccount?.id}'),
+          trailing:  PopupMenuButton<String>(
+              onSelected: (value) {
+                if (value == 'update') {
+                // Handle the "Update" option
+                  print("updarred");
+                } else if (value == 'delete') {
+                // Handle the "Delete" option
+                  print("deeleeeted");
+                }
+              },
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                  const PopupMenuItem<String>(
+                      value: 'update',
+                      child: ListTile(
+                        contentPadding: EdgeInsets.zero,
+                        leading: Icon(Icons.edit),
+                        title: Text('Update'),
+                      ),
+                  ),
+                  const PopupMenuItem<String>(
+                    value: 'delete',
+                    child: ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      leading: Icon(Icons.delete),
+                      title: Text('Delete'),
+                    ),
+                  ),
+              ],
+          ),
           onTap: () {
             Navigator.pushNamed(context, '/transactionList',arguments: bankAccount?.id);
             print('Go to transaction page and provide bankaccount ID for firestore');
           },
+
         ),
       ),
     );
